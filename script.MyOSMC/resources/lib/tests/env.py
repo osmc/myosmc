c3b0be5ec109385a4c35d67ad722bf84752b7bea
KODeKarnage <subliminal.karnage@gmail.com>
2017-05-11 09:55:49 +1000

    tests: update env file to mock the 4 unavailable modules

--- script.MyOSMC/resources/lib/tests/env.py
+++ script.MyOSMC/resources/lib/tests/env.py
@@ -1,8 +1,14 @@
-import sys
 import os
+import sys
+
+from mock import Mock
 
 # this appends the lib folder to system path
 # you will need to refer to underlying modules directly in your test imports
 # for example, from database.dbinterface import DBInterface
-sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+sys.modules['xbmc'] = Mock()
+sys.modules['xbmcaddon'] = Mock()
+sys.modules['xbmcgui'] = Mock()
+sys.modules['xbmccvfs'] = Mock()
