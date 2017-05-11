c3fe6a0419560da5c9fbc0f30e6765a7a3a623c6
KODeKarnage <subliminal.karnage@gmail.com>
2017-05-11 16:55:43 +1000

    common tools: fixed __init__ import error

--- script.MyOSMC/resources/lib/common/__init__.py
+++ script.MyOSMC/resources/lib/common/__init__.py
@@ -1,3 +1,6 @@
-from common_tools import *
+from logger import Logger
+from language import Translator
+from hardware_version import hardware_version
+from string_manipulation import sanitize_string
 from OpenWithBackup import OpenWithBackup
 __all__ = ['hardware_version', 'sanitize_string','language','logger','OpenWithBackup']
