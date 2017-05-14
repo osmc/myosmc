722367891b713fb9a036324cfcc154f75757f8c3
KODeKarnage <subliminal.karnage@gmail.com>
2017-05-15 09:47:22 +1000

    removed old file, renamed configfileinterface

--- script.MyOSMC/resources/lib/piconfig/ConfigFileInterface.py
+++ script.MyOSMC/resources/lib/piconfig/ConfigFileInterface.py
@@ -5,220 +5,210 @@ from piSettings import PassThrough, CLASS_LIBRARY
 
 class ConfigFileInterface(object):
 
-	def __init__(self, location='/boot/config.txt'):
+    def __init__(self, location='/boot/config.txt'):
 
-		self.location = location
-		self.OpenWithBackup = OpenWithBackup
+        self.location = location
+        self.OpenWithBackup = OpenWithBackup
 
+    def _clean_this_line(self, original_line):
 
-	def _clean_this_line(self, original_line):
+        clean_line = original_line
 
-		clean_line = original_line
+        clean_line = clean_line.strip()
 
-		clean_line = clean_line.strip()
+        if not clean_line:
+            return ''
 
-		if not clean_line:
-			return ''
+        # ignore commented out lines
+        if clean_line.startswith('#'):
+            return ''
 
-		# ignore commented out lines
-		if clean_line.startswith('#'):
-			return ''
+        # strip the line of any inline comments
+        if '#' in clean_line:
+            clean_line = clean_line[:clean_line.index('#')]
 
-		# strip the line of any inline comments
-		if '#' in clean_line: 
-			clean_line = clean_line[:clean_line.index('#')]
+            # restrip the line
+            clean_line = clean_line.strip()
 
-			# restrip the line
-			clean_line = clean_line.strip()
+        return clean_line
 
-		return clean_line
+    def _clean_this_doc(self, doc):
 
+        # reverse the lines in the doc.
+        # the config.txt needs to be read from the bottom up
+        doc = doc[::-1]
 
-	def _clean_this_doc(self, doc):
+        clean_doc = []
+        for original_line in doc:
 
-		# reverse the lines in the doc. 
-		# the config.txt needs to be read from the bottom up
-		doc = doc[::-1]
+            clean = self._clean_this_line(original_line)
 
-		clean_doc = []
-		for original_line in doc:
-			
-			clean = self._clean_this_line(original_line)
-			
-			clean_doc.append( {'original': original_line, 'clean':clean, 'setting':None} )
+            clean_doc.append({'original': original_line, 'clean': clean, 'setting': None})
 
-		return clean_doc
+        return clean_doc
 
+    def _append_unmatched_settings_to_doc(self, clean_doc, _settings):
+        # run through the settings again and add any that have not been assigned
+        #  to the document with their default values
+        for setting in _settings:
 
-	def _append_unmatched_settings_to_doc(self, clean_doc, _settings):
-		# run through the settings again and add any that have not been assigned
-		#  to the document with their default values
-		for setting in _settings:
+            if setting.foundinDoc:
+                continue
 
-			if setting.foundinDoc:
-				continue
+            setting.set_current_value_to_default()
 
-			setting.set_current_value_to_default()
+            clean_doc.append({'original': 'NULL', 'clean': 'NULL', 'setting': setting})
 
-			clean_doc.append( {'original': 'NULL', 'clean':'NULL', 'setting':setting} )		
+        return clean_doc
 
-		return clean_doc
+    def _assign_settings_to_doc(self, clean_doc, _settings):
+        '''
+        Goes through the clean doc, assigns a piSetting to each line.
+        Settings that are not added to a line are added to the end of the document
+        with their default values.
+        This ensures that all settings in MASTERSETTINGS are represented in the final doc.
+        '''
 
+        for config_line in clean_doc:
+            print '#' + config_line['clean']
 
-	def _assign_settings_to_doc(self, clean_doc, _settings):
-		'''
-		Goes through the clean doc, assigns a piSetting to each line.
-		Settings that are not added to a line are added to the end of the document
-		with their default values.
-		This ensures that all settings in MASTERSETTINGS are represented in the final doc.
-		'''
+            # check the config_line against all the settings, exiting loop on first valid find
+            for setting in _settings:
+                try:
+                    setting = setting.extract_setting_from_line(config_line)
+                    config_line['setting'] = setting
 
-		for config_line in clean_doc:
-			print '#' + config_line['clean']
+                    symbol = '==' if str(setting.default_value) == str(setting.current_config_value) else '!='
 
-			# check the config_line against all the settings, exiting loop on first valid find
-			for setting in _settings:
-				try:
-					setting = setting.extract_setting_from_line( config_line )
-					config_line['setting'] = setting
-					
-					symbol = '==' if str(setting.default_value) == str(setting.current_config_value) else '!='
+                    print 'Assigning -- %s %s %s \n' % (setting.default_value, symbol, setting.current_config_value)
 
-					print 'Assigning -- %s %s %s \n' % (setting.default_value, symbol, setting.current_config_value)
+                    break  # go to the next config_line
+                except ValueError:
+                    pass
 
-					break  # go to the next config_line
-				except ValueError:
-					pass
+            else:  # if no break
+                # passthrough the original line to the final document
+                print 'passing through\n'
+                config_line['setting'] = PassThrough(name='passthrough')
 
-			else: # if no break
-				# passthrough the original line to the final document
-				print 'passing through\n'
-				config_line['setting'] = PassThrough(name='passthrough')
+        return clean_doc
 
-		return clean_doc
+    def extract_settings_from_doc(self, final_doc):
 
+        return {config_line['setting'].name: config_line['setting'].current_config_value for config_line in final_doc}
 
-	def extract_settings_from_doc(self, final_doc):
+    def _generate_list_of_settings(self):
+        '''
+            Builds the library of Settings instances. These are used against each line in the
+            config.txt, with the first match being assigned as the Setting for that line.
+        '''
+        _settings = []
 
-		return { config_line['setting'].name: config_line['setting'].current_config_value for config_line in final_doc}
+        for key, attributes in MASTER_SETTINGS.iteritems():
 
+            typ = attributes['type']
+            piclass = CLASS_LIBRARY[typ]
+            setting = piclass(name=key)
 
-	def _generate_list_of_settings(self):
-		'''
-			Builds the library of Settings instances. These are used against each line in the 
-			config.txt, with the first match being assigned as the Setting for that line.
-		'''
-		_settings = []
+            setting.set_stub(attributes['stub'])
+            setting.set_default_value(attributes['default'])
+            setting.set_valid_values(attributes['valid'])
 
-		for key, attributes in MASTER_SETTINGS.iteritems():
+            for x in attributes['patterns']:
+                setting.add_pattern(x['id_pattern'], x['ext_pattern'])
 
-			typ = attributes['type']
-			piClass = CLASS_LIBRARY[typ]
-			setting = piClass(name=key)
+            _settings.append(setting)
 
-			setting.set_stub(attributes['stub'])
-			setting.set_default_value(attributes['default'])
-			setting.set_valid_values(attributes['valid'])
+        return _settings
 
-			for x in attributes['patterns']:
-				setting.add_pattern(x['id_pattern'], x['ext_pattern'])
+    def read_config_txt(self):
+        '''
+        Reads the config.txt found at the provided location and produces a list of config_lines.
+        config_lines are dicts containing:
+        - the original line from the config.txt
+        - a cleaned up version of that line
+        - the order the line is found in the config
+        - a piSetting instance that has the retrieved validated value
 
-			_settings.append(setting)
+        The final doc contains what will eventually be written to the new config.txt
+        '''
 
-		return _settings
+        # first step is to use the Master_Settings information to create a list of piSetting instances
+        _settings = self._generate_list_of_settings()
 
+        with open(self.location, 'r') as f:
+            dirty_doc = f.readlines()
 
-	def read_config_txt(self):
-		'''
-		Reads the config.txt found at the provided location and produces a list of config_lines.
-		config_lines are dicts containing:
-		- the original line from the config.txt
-		- a cleaned up version of that line
-		- the order the line is found in the config
-		- a piSetting instance that has the retrieved validated value
+        clean_doc = self._clean_this_doc(dirty_doc)
 
-		The final doc contains what will eventually be written to the new config.txt
-		'''
+        clean_doc = self._assign_settings_to_doc(clean_doc, _settings)
 
-		# first step is to use the Master_Settings information to create a list of piSetting instances
-		_settings = self._generate_list_of_settings()
+        final_doc = self._append_unmatched_settings_to_doc(clean_doc, _settings)
 
-		with open(self.location, 'r') as f:
-			dirty_doc = f.readlines()
+        return final_doc
 
-		clean_doc = self._clean_this_doc( dirty_doc )
+    def write_config_txt(self, final_doc):
+        ''' Backs up the existing config.txt
+        Runs through the final doc producing a list of lines to write back to a new config.txt
+        '''
 
-		clean_doc = self._assign_settings_to_doc( clean_doc, _settings )
+        new_lines = []
 
-		final_doc = self._append_unmatched_settings_to_doc(clean_doc, _settings)
+        for config_line in final_doc:
+            setting = config_line['setting']
 
-		return final_doc
+            # settings that are the default values, and where defaults are suppressed should be ignored
+            # (i.e. dont write them to the new config.txt)
+            if setting.isDefault and setting.suppress_defaults:
+                continue
 
+            # settings that are not changed should just have the original line replicated in the new config.txt
+            if not setting.isChanged:
+                new_lines.append(config_line['original'])
 
-	def write_config_txt(self, final_doc, OpenWithBackup=None):
-		''' Backs up the existing config.txt
-		Runs through the final doc producing a list of lines to write back to a new config.txt
-		'''
+            # lines for which the values have changed should have the final_line brought in from the piSetting
+            new_lines.append(setting.final_line)
 
-		new_lines = []
+        # reverse the lines back to the original order
+        new_lines = new_lines[::-1]
 
-		for config_line in final_doc:
-			setting = config_line['setting']
+        if self.OpenWithBackup:
+            with self.OpenWithBackup(self.location, 'w') as f:
+                f.writelines(new_lines)
+        else:
+            with open(self.location, 'w') as f:
+                f.writelines(new_lines)
 
-			# settings that are the default values, and where defaults are suppressed should be ignored
-			# (i.e. dont write them to the new config.txt)
-			if setting.isDefault and setting.suppress_defaults:
-				continue
+    def update_settings(self, final_doc, new_settings):
 
-			# settings that are not changed should just have the original line replicated in the new config.txt
-			if not setting.isChanged:
-				new_lines.append(config_line['original'])
+        for config_line in final_doc:
 
-			# lines for which the values have changed should have the final_line brought in from the piSetting
-			new_lines.append(setting.final_line)
+            setting = config_line['setting']
 
-		# reverse the lines back to the original order
-		new_lines = new_lines[::-1]
+            setting.set_new_value(new_settings[setting.name])
 
-		if self.OpenWithBackup:
-			with self.OpenWithBackup(self.location, 'w') as f:
-				f.writelines(new_lines)
-		else:
-			with open(self.location, 'w') as f:
-				f.writelines(new_lines)
-
-
-	def update_settings(self, final_doc, new_settings):
-
-		for config_line in final_doc:
-			
-			setting = config_line['setting']
-
-			setting.set_new_value( new_settings[ setting.name ] )
-
-		return final_doc
-
+        return final_doc
 
 
 if __name__ == "__main__":
 
-	import sys
-	from pprint import pprint
+    import sys
+    from pprint import pprint
 
-	sys.stdout = open('C:\\t\\logfile', 'w')
+    sys.stdout = open('C:\\t\\logfile', 'w')
 
-	c = ConfigFileInterface('samples\\config_05.txt')
+    c = ConfigFileInterface('samples\\config_05.txt')
 
-	doc = c.read_config_txt()
+    doc = c.read_config_txt()
 
-	res = c.extract_settings_from_doc(doc)
+    res = c.extract_settings_from_doc(doc)
 
-	print '\n\n'
+    print '\n\n'
 
-	pprint(res)
-	print '\n\n'
+    pprint(res)
+    print '\n\n'
 
-	for x in doc:
-		print x['clean']
-		pprint(x['setting'].__dict__)
-		print '\n'
+    for x in doc:
+        print x['clean']
+        pprint(x['setting'].__dict__)
+        print '\n'
