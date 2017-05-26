92afed33fd0312de6af62a26fd53ec4c341fe04b
KODeKarnage <subliminal.karnage@gmail.com>
2017-05-26 15:48:04 +1000

    Logger: fixed whodidis issue., removed instance variable and now show classname in log

--- script.MyOSMC/resources/lib/common/logger.py
+++ script.MyOSMC/resources/lib/common/logger.py
@@ -7,10 +7,10 @@ class Logger(object):
 
     def __init__(self):
 
-        self.whodisis = self.__class__.__name__
+        pass
 
     def log(self, raw_message, level=xbmc.LOGDEBUG):
 
         message = sanitize_string(raw_message)
 
-        xbmc.log(self.whodisis + ' ' + message, level=level)
+        xbmc.log(self.__class__.__name__ + ' ' + message, level=level)
