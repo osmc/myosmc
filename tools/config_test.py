eb70c5888537da6066c946b9fb3c435ca38f39ef
KODeKarnage <sublime.kev@gmail.com>
2018-07-01 10:23:15 +1000

    created pi config tester/reporter, can be used to check the results of the piconfig interface on specific config files

---
+++ tools/config_test.py
@@ -0,0 +1,48 @@
+from mock import Mock
+from pprint import pprint
+import os
+import sys
+
+
+base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.append(base + '/script.MyOSMC')
+
+samples = base + '/tests/test_data/public_configs/'
+
+sys.modules['xbmc'] = Mock()
+sys.modules['xbmcaddon'] = Mock()
+sys.modules['xbmcgui'] = Mock()
+sys.modules['xbmccvfs'] = Mock()
+
+from resources.lib.piconfig.configfileinterface import ConfigFileInterface
+
+with open(samples + 'config_14.txt', 'r') as f:
+  orig_lines = f.readlines()
+
+cfi = ConfigFileInterface(location=samples + 'config_14.txt' )
+res = cfi.read_config_txt()
+
+fin = [x for x in res if all([x['clean'].strip(), x['clean'] != 'NULL'])]
+
+fin_orig = [x['original'] for x in fin]
+
+passthroughs = [x['original'] for x in fin if 'passthrough' in str(x['setting'])]
+
+fin = [x for x in fin if x['original'] not in passthroughs]
+
+for x in fin:
+    pprint(x)
+    print '\n'
+
+missing = []
+for line in orig_lines:
+    if line not in fin_orig:
+        if line.strip():
+            missing.append(line)
+
+if passthroughs:
+    print '\nLINES PASSED THROUGH: \n\t%s' % '\t'.join(passthroughs)
+
+if missing:
+    print '\nLINES MISSING: \n\t%s' % '\t'.join(missing)
