a12a3337231aca51c4dc7ba6a00180ccd3a192ef
KODeKarnage <sublime.kev@gmail.com>
2018-07-08 13:45:54 +1000

    Fixed hardwareversion reference in __init__ for common module

--- script.MyOSMC/resources/lib/common/__init__.py
+++ script.MyOSMC/resources/lib/common/__init__.py
@@ -1,6 +1,6 @@
 from logger import Logger
 from language import Translator
-from hardwareversion import hardware_version
+from hardware import get_proc_info, get_Pi_clock_settings
 from string_manipulation import sanitize_string
 from openwithbackup import OpenWithBackup
-__all__ = ['hardware_version', 'sanitize_string','language','logger','OpenWithBackup']
+__all__ = ['hardware', 'sanitize_string','language','logger','OpenWithBackup']
